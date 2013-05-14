96f033762ff33833d6d25994467149a1b1f1bdde
Robbie Vanbrabant <robbie.vanbrabant@gmail.com>
2013-05-14 13:41:40 +0100

    evil tweet parsing

--- pyrate/pyrate.py
+++ pyrate/pyrate.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python
+import re
 from twitter_access import *
 
 pirate = Pirate()
+PLACE_HOLDER = 140 * '%'
+
+def place_holders(count):
+  for i in range(0, count):
+    yield PLACE_HOLDER + str(i)
+
+# myString = 
+def parse_tweet(text):
+  urls = re.findall(r'(https?://[^\s]+)', text)
+  users = re.findall(r'@[a-zA-Z\-_]+', text)
+  total = set(urls + users)
+  replacements = dict(zip(place_holders(len(total)), total))
+
+  for key, value in replacements.items():
+    text = re.sub(value, key, text)
+
+  print text
+
+  print replacements
 
 def run():
   factory = TwitterFactory()
   twitter = factory.create()
 
-  tweets = twitter.statuses.user_timeline()
+  tweets = twitter.statuses.user_timeline(include_rts=True)
 
   for t in tweets:
     print pirate.speak(t['text'])
@@ -21,4 +41,4 @@ def run_stream():
       print pirate.speak(t['text'])
 
 if __name__ == "__main__":
-  run_stream() 
+  parse_tweet("This is my tweet for @robbiev check it out http://tinyurl.com/blah")
