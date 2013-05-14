ecc2e375a2adacf9511c0f6382f777a43e70ded0
Robbie Vanbrabant <robbie.vanbrabant@gmail.com>
2013-05-14 12:20:08 +0100

    investigate twitter streaming api

--- pyrate/pyrate.py
+++ pyrate/pyrate.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python
 from twitter_access import *
 
+pirate = Pirate()
+
 def run():
-  api = TwitterApi()
-  twitter = api.create()
+  factory = TwitterFactory()
+  twitter = factory.create()
+
   tweets = twitter.statuses.user_timeline()
-  pirate = Pirate()
+
   for t in tweets:
     print pirate.speak(t['text'])
 
+def run_stream():
+  factory = TwitterFactory()
+  streamer = factory.create_stream()
+  tweets = streamer.statuses.sample()
+  for t in tweets:
+    if t.get('text'):
+      print pirate.speak(t['text'])
+
 if __name__ == "__main__":
-  run() 
+  run_stream() 
--- pyrate/twitter_access.py
+++ pyrate/twitter_access.py
@@ -5,28 +5,25 @@ from twitter import *
 
 from speech import *
 
-class TwitterApi:
+class TwitterFactory:
   def __init__(self):
     self.consumer_key = 'teXFL6FkI2nE2KeBYWNs1A'
     self.consumer_secret = 'KdnLaOXH3kQnffcg07UgkT0IFQC9wepvmXmqU2LENo'
     self.my_twitter_creds = os.path.expanduser('~/.pyrate_credentials')
 
-  def create(self):
+  def oauth(self):
     if not os.path.exists(self.my_twitter_creds):
       oauth_dance('pyrate', self.consumer_key, self.consumer_secret, self.my_twitter_creds)
 
     oauth_token, oauth_secret = read_token_file(self.my_twitter_creds)
+    return OAuth(oauth_token, oauth_secret, self.consumer_key, self.consumer_secret)
 
-    return Twitter(auth=OAuth(oauth_token, oauth_secret, self.consumer_key,
-      self.consumer_secret))
+  def create(self):
+    return Twitter(auth=self.oauth())
+
+  def create_stream(self):
+    return TwitterStream(auth=self.oauth())
 
   def rate_limit_status(self, twitter):
     r = twitter.application.rate_limit_status()
     print r
-
-class TwitterCloner:
-  def __init__(self, twitter):
-    self.twitter = twitter
-
-  def clone(self):
-    pass
